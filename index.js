21eb0c178ecad04835d2ebdeb803ea58d9eac53b
Széles Máté <szelessm@gmail.com>
2021-11-18 10:59:13 +0100

    display puzzle

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import { RandomWord } from './RandomWord.js';
+import { Hangman } from './Hangman.js';
 
 document.querySelector('#start').addEventListener('click', start);
 
 async function start() {
     let rndWord = new RandomWord;
     let word = await rndWord.randomWord()
+    let game = new Hangman(word, word.length);
+
     console.log(word);
+    document.querySelector('#word').innerHTML = game.puzzle;
 }
